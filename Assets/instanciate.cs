954459aa74c80f0872a478789056f7c99fcad57b
Deleval <elise.deleval@hotmail.fr>
2021-01-21 00:59:16 +0100

    up exo
    up exo

---
+++ Assets/instanciate.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class instanciate : MonoBehaviour
+{
+    private Transform sphererHolder;
+    [SerializeField] private int numberofSphere = 4;
+    [SerializeField] private Vector3 rotationCenter = default;
+    [SerializeField] private GameObject cubePrefab = default;
+    [SerializeField] private float Radius= 9f;
+    [SerializeField] private Vector3 position = default;
+
+    private Transform parent;
+    // Start is called before the first frame update
+    void Start()
+    {   
+        sphererHolder = transform;
+        StartCoroutine(Instancier());
+        parent = GameObject.Find("Capsule").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public IEnumerator Instancier()
+    {
+        while (true)
+        {
+
+            foreach(Transform child in sphererHolder) Destroy(child.gameObject);
+            for (int i = 0; i < numberofSphere;i++)
+            { 
+                position = new Vector3(rotationCenter.x+ Radius * Mathf.Cos(2 * Mathf.PI * i / numberofSphere), rotationCenter.y + Radius * Mathf.Sin(2 * Mathf.PI * i / numberofSphere), rotationCenter.z);
+                Vector3 direction = new Vector3(
+                    position.x - transform.position.x, 
+                    position.y - this.transform.position.y,
+                    position.z - this.transform.position.z);
+                GameObject truc= (GameObject) Instantiate(cubePrefab, position, Quaternion.identity,sphererHolder);
+            }
+            
+
+            
+            yield return new WaitForSeconds(5f);
+        }
+    }
+}
---
+++ Assets/oui.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class oui : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {   
+        
+        Transform child = transform;
+        Transform parent = GameObject.Find("Capsule").transform;
+        Debug.Log(parent.position.x);
+        Vector3 direction = new Vector3(
+            child.position.x-parent.position.x , 
+            child.position.y-parent.position.y ,
+            child.position.z-parent.position.z );
+        Quaternion m_MyQuaternion;
+        m_MyQuaternion = new Quaternion();
+        m_MyQuaternion.SetFromToRotation(Vector3.up, direction);
+        child.position = Vector3.Lerp(child.position, transform.position, 1 * Time.deltaTime);
+        child.rotation = m_MyQuaternion * child.rotation;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
